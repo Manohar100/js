73ab9fd400a87a7085ca1c1ffcfad754c709ad42
MANOHAR KUMAR JHA <123536144+Manohar100@users.noreply.github.com>
2023-09-13 16:48:11 +0000

    dataypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,2 +1,17 @@
 "use strict"; //treat all JS code as newer version of javascript
-alert(3+3)
+// alert(3+3) we are using node.js not browser
+let name="manohar"
+let age="22"
+let isloggedin=false
+
+
+/* number datatypes => 2 to power of 53*/
+// bigint 
+// string => ""
+//boolean => true ,false
+// null => standalone value
+// undefined =>
+// symbol => unique
+//object 
+console.log(typeof "manohar");
+console.log(typeof null);
