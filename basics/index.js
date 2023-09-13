024bd4b494451ad617070a7296afcec40f0f71f5
MANOHAR KUMAR JHA <123536144+Manohar100@users.noreply.github.com>
2023-09-13 16:16:16 +0000

    configure config

--- basics/index.js
+++ basics/index.js
@@ -1,1 +1,11 @@
 console.log("Hello");
+const accountId =123444;
+let accountEmail ="fdjs@gmail.com";
+accountEmail="djfs@gmail.com";
+var accountPassword ="12334";
+let accountState;
+accountPassword="55555"
+accountCity = "Jaipur"
+accountCity="bhopal"
+/* accountId = 23; not possible */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
